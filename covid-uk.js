3568a7b11a4788f924b975756936f32bfc676a6d
Mark Raymond <markr@raymonds.org.uk>
2021-03-28 14:52:34 +0100

    A bit more variable safety :)

--- covid-uk.js
+++ covid-uk.js
@@ -4,8 +4,8 @@ window.onload = function() {
     .then(response => response.json())
     .then(rawData => {
       function drawChart(id, datasets) {
-        var ctx = document.getElementById(id).getContext('2d');
-        var chart = new Chart(ctx, {
+        const ctx = document.getElementById(id).getContext('2d');
+        const chart = new Chart(ctx, {
           type: 'line',
           data: {
             datasets: datasets
@@ -24,22 +24,22 @@ window.onload = function() {
         });
         return chart;
       };
-      var cases = rawData.data.map(function(day) {
+      const cases = rawData.data.map(function(day) {
         return {x: new Date(day.date), y: day.cases};
       });
-      var admissions = rawData.data.map(function(day) {
+      const admissions = rawData.data.map(function(day) {
         return {x: new Date(day.date), y: day.admissions};
       });
-      var deaths = rawData.data.map(function(day) {
+      const deaths = rawData.data.map(function(day) {
         return {x: new Date(day.date), y: day.deaths};
       });
-      var firstDoses = rawData.data.map(function(day) {
+      const firstDoses = rawData.data.map(function(day) {
         return {x: new Date(day.date), y: day.firstDoses};
       });
-      var secondDoses = rawData.data.map(function(day) {
+      const secondDoses = rawData.data.map(function(day) {
         return {x: new Date(day.date), y: day.secondDoses};
       });
-      var charts = [
+      const charts = [
         drawChart('cases', [{
           label: 'Cases',
           data: cases
@@ -61,8 +61,8 @@ window.onload = function() {
         }])
       ];
       document.getElementById('redraw').onclick = () => {
-        for (var i = 0; i < charts.length; i++) {
-          var xaxis = charts[i].options.scales.xAxes[0];
+        for (let i = 0; i < charts.length; i++) {
+          const xaxis = charts[i].options.scales.xAxes[0];
           xaxis.ticks.min = new Date(document.getElementById('from').value);
           xaxis.ticks.max = new Date(document.getElementById('to').value);
           charts[i].update();
